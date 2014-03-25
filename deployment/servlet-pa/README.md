043d4ca236a807cb6abc4b3e70b19713b0d341bf
Daniel Meyer <daniel.meyer@camunda.com>
2014-03-25 16:12:34 +0100

    feat(deployment/servlet-pa): provide example
    add example for simple servlet process application

---
+++ deployment/servlet-pa/README.md
@@ -0,0 +1,17 @@
+Simple Servlet Process Application Example
+==========================================
+
+This example demonstrates how to deploy a simple servlet process application.
+
+Built and tested against camunda BPM version `7.1.0-SNAPSHOT`.
+
+
+How to use it
+-----------------------------
+
+1. Checkout the project with Git
+2. Import into your IDE
+3. Build it with maven
+4. Deploy it to a camunda-bpm-platform distro at one's own choice
+6. Checkout the console if you can find: 'Service Task 'Example ServiceTask' is invoked!'
+
---
+++ deployment/servlet-pa/src/main/java/org/camunda/bpm/platform/example/servlet/ExampleServiceTask.java
@@ -0,0 +1,45 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.platform.example.servlet;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.model.bpmn.instance.FlowElement;
+
+/**
+ * This is an implementation of the {@link JavaDelegate} interface. Implemenations of that class can
+ * be directly invoked by the process engine while executing a BPMN process.
+ *
+ * @author Daniel Meyer
+ *
+ */
+public class ExampleServiceTask implements JavaDelegate {
+
+  public final static Logger LOGGER = Logger.getLogger(ExampleServiceTask.class.getName());
+
+  /**
+   * this method will be invoked by the process engine when the service task is executed.
+   */
+  public void execute(DelegateExecution execution) throws Exception {
+
+    // use camunda model API to get current BPMN element
+    FlowElement serviceTask = execution.getBpmnModelElementInstance();
+
+    // log status
+    LOGGER.info("\n\n\nService Task '"+serviceTask.getName() +"' is invoked!\n\n\n");
+
+  }
+
+}
