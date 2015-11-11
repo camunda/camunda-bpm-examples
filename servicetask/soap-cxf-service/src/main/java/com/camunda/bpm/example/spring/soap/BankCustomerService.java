bf197719067f8f3bd98a4ed618bfdc575e61f329
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2015-11-11 23:40:22 +0100

    improve(examples): Improve soap cxf example

--- servicetask/soap-cxf-service/src/main/java/com/camunda/bpm/example/spring/soap/BankCustomerService.java
+++ servicetask/soap-cxf-service/src/main/java/com/camunda/bpm/example/spring/soap/BankCustomerService.java
@@ -1,9 +1,16 @@
 package com.camunda.bpm.example.spring.soap;
 
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.impl.pvm.runtime.ExecutionImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.remoting.soap.SoapFaultException;
 import org.springframework.stereotype.Service;
 
+import com.camunda.bpm.example.spring.soap.v1.BankException;
+import com.camunda.bpm.example.spring.soap.v1.BankException_Exception;
 import com.camunda.bpm.example.spring.soap.v1.BankRequestHeader;
 import com.camunda.bpm.example.spring.soap.v1.GetAccountsRequest;
 import com.camunda.bpm.example.spring.soap.v1.GetAccountsResponse;
@@ -17,6 +24,8 @@ import com.camunda.bpm.example.spring.soap.v1.GetAccountsResponse;
 @Service("bankCustomerService")
 public class BankCustomerService {
 
+  private static Logger log = Logger.getLogger(BankCustomerService.class.getName());
+
   private BankCustomerClientService service;
 
   @Autowired
@@ -24,15 +33,27 @@ public class BankCustomerService {
     this.service = service;
   }
 
-  public GetAccountsResponse getAccounts(GetAccountsRequest request, DelegateExecution execution) throws Exception {
+  public GetAccountsResponse getAccounts(DelegateExecution execution) throws Exception {
+    
+    GetAccountsRequest request = new GetAccountsRequest();
+    request.setCustomerNumber((String)execution.getVariable("customerNumber"));
+    
     BankRequestHeader requestHeader = getSecurityHeader(execution);
 
-    return service.getAccounts(request, requestHeader);
+    try {
+      return service.getAccounts(request, requestHeader);
+    } catch(BankException_Exception e) {
+      
+      BankException faultInfo = e.getFaultInfo();
+      log.warning("Problem getting accounts: " + faultInfo.getCode() + ": " + faultInfo.getMessage());
+      throw new BpmnError("serviceException");
+    }
+    
   }
 
   private BankRequestHeader getSecurityHeader(DelegateExecution execution) {
     BankRequestHeader requestHeader = new BankRequestHeader();
-    requestHeader.setSingleSignonToken(execution.getVariable("singleSignonToken").toString());
+    requestHeader.setSecret(execution.getVariable("secret").toString());
     return requestHeader;
   }
 
