1531f524f2b91a90313c57d88ee96e16285f540d
Sebastian Menski <sebastian@menski.org>
2015-11-23 16:24:44 +0100

    feat(decision-engine): add Dish Decision Maker example
    related to #CAM-5032

---
+++ decision-engine/dish-decision-maker/src/main/java/org/camunda/bpm/example/DishDecisionMaker.java
@@ -0,0 +1,69 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camunda.bpm.example;
+
+import java.io.InputStream;
+
+import org.camunda.bpm.dmn.engine.DmnDecision;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.dmn.engine.DmnEngine;
+import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+
+public class DishDecisionMaker {
+
+  public static void printUsage(String errorMessage, int exitCode) {
+    System.err.println("Error: " + errorMessage);
+    System.err.println("Usage: java -jar DishDecisionMaker.jar SEASON GUEST_COUNT\n\n\tSEASON: the current season (Spring, Summer, Fall or Winter)\n\tGUEST_COUNT: number of guest to expect");
+    System.exit(exitCode);
+  }
+
+  public static void main(String[] args) {
+    // parse arguments
+    if (args.length != 2) {
+      printUsage("Please specify the current season and the expected number of guests.", 1);
+    }
+    String season = args[0];
+
+    Integer guestCount = 0;
+    try  {
+      guestCount = Integer.parseInt(args[1]);
+    }
+    catch (NumberFormatException e) {
+      printUsage("The number of expected guests must be a number", 2);
+    }
+
+    // prepare variables for decision evaluation
+    VariableMap variables = Variables
+      .putValue("season", season)
+      .putValue("guestCount", guestCount);
+
+    // create a new default DMN engine
+    DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+
+    // parse decision from resource input stream
+    InputStream inputStream = DishDecisionMaker.class.getResourceAsStream("dish-decision.dmn11.xml");
+    DmnDecision decision = dmnEngine.parseDecision("decision", inputStream);
+
+    // evaluate decision
+    DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+
+    // print result
+    String desiredDish = result.getSingleResult().getSingleEntry();
+    System.out.println("Dish Decision:\n\tI would recommend to serve: " + desiredDish);
+  }
+
+
+}
---
+++ decision-engine/dish-decision-maker/src/test/java/org/camunda/bpm/example/DishDecisionTest.java
@@ -0,0 +1,76 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.camunda.bpm.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+
+import org.camunda.bpm.dmn.engine.DmnDecision;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.dmn.engine.DmnEngine;
+import org.camunda.bpm.dmn.engine.test.DmnEngineRule;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class DishDecisionTest {
+
+  @Rule
+  public DmnEngineRule dmnEngineRule = new DmnEngineRule();
+
+  public DmnEngine dmnEngine;
+  public DmnDecision decision;
+
+  @Before
+  public void parseDecision() {
+    InputStream inputStream = getClass().getResourceAsStream("dish-decision.dmn11.xml");
+    dmnEngine = dmnEngineRule.getDmnEngine();
+    decision = dmnEngine.parseDecision("decision", inputStream);
+  }
+
+  @Test
+  public void shouldServeDryAgedInSpringForFewGuests() {
+    VariableMap variables = Variables
+      .putValue("season", "Spring")
+      .putValue("guestCount", 4);
+
+    DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+    assertEquals("Dry Aged Gourmet Steak", result.getSingleResult().getSingleEntry());
+  }
+
+  @Test
+  public void shouldServeSteakInSpringForSomeGuests() {
+    VariableMap variables = Variables
+      .putValue("season", "Spring")
+      .putValue("guestCount", 7);
+
+    DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+    assertEquals("Steak", result.getSingleResult().getSingleEntry());
+  }
+
+  @Test
+  public void shouldServeStewInSpringForMayGuests() {
+    VariableMap variables = Variables
+      .putValue("season", "Spring")
+      .putValue("guestCount", 20);
+
+    DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+    assertEquals("Stew", result.getSingleResult().getSingleEntry());
+  }
+
+}
