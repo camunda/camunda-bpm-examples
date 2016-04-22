631b4b5567289c7dfae6da37ca33c28cdbd14a17
Jan Galinski <jan.galinski@holisticon.de>
2016-04-22 11:54:34 +0200

    add classpth:/static/ to resourceHandler /**, fixes #68

--- camunda-spring-boot-starter-example-webapp/src/main/java/org/camunda/bpm/spring/boot/starter/example/webapp/WebappExampleApplication.java
+++ camunda-spring-boot-starter-example-webapp/src/main/java/org/camunda/bpm/spring/boot/starter/example/webapp/WebappExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebappExampleApplication {
 
-  public static void main(String[] args) {
+  public static void main(String... args) {
     SpringApplication.run(WebappExampleApplication.class, args);
   }
 }
