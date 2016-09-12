add9b29623d20d9acbea6c6928047b889721619b
Oliver Steinhauer <oliver.steinhauer@gmail.com>
2016-09-12 08:35:57 +0200

    catch InterruptedException (hopefully) fixes #144

--- example-cloud/src/test/java/org/camunda/bpm/spring/boot/starter/example/cloud/simple/SimpleApplicationTest.java
+++ example-cloud/src/test/java/org/camunda/bpm/spring/boot/starter/example/cloud/simple/SimpleApplicationTest.java
@@ -2,6 +2,8 @@ package org.camunda.bpm.spring.boot.starter.example.cloud.simple;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Timed;
@@ -11,14 +13,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = { SimpleApplication.class })
 public class SimpleApplicationTest {
 
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
   @Autowired
   private SimpleApplication application;
 
   @Test
   @Timed(millis = 10 * 1000)
-  public void would_fail_if_process_not_completed_after_10_seconds() throws InterruptedException {
+  public void would_fail_if_process_not_completed_after_10_seconds() {
     while (!application.contextClosed && !application.isProcessInstanceFinished()) {
-      Thread.sleep(500L);
+      try {
+        Thread.sleep(500L);
+      } catch (InterruptedException e) {
+        logger.debug("interrupted, that is fine: {}", e.getMessage());
+      }
     }
   }
 }
