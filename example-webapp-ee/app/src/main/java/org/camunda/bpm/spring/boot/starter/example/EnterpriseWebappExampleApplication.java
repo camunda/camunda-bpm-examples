43523636cd9d929d5cbea1592b4fd77023db454a
Jan Galinski <jan.galinski@holisticon.de>
2016-11-22 06:53:01 +0100

    Enter license (#173)
    * enter license wip
    
    * enter license key for enterprise webapp, fixes #172
    
    * enter license - disable ee-example

---
+++ example-webapp-ee/app/src/main/java/org/camunda/bpm/spring/boot/starter/example/EnterpriseWebappExampleApplication.java
@@ -0,0 +1,14 @@
+package org.camunda.bpm.spring.boot.starter.example;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class EnterpriseWebappExampleApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(EnterpriseWebappExampleApplication.class, args);
+  }
+}
