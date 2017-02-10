487a4d397c66044c55343a073e6af344d515e9c6
Jan Galinski <jan.galinski@holisticon.de>
2017-02-10 16:46:40 +0100

    add enableProcessApplication to example-web, fixes #183

--- example-web/src/main/java/org/camunda/bpm/spring/boot/example/web/RestApplication.java
+++ example-web/src/main/java/org/camunda/bpm/spring/boot/example/web/RestApplication.java
@@ -1,9 +1,11 @@
 package org.camunda.bpm.spring.boot.example.web;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class RestApplication {
 
   public static void main(final String... args) throws Exception {
