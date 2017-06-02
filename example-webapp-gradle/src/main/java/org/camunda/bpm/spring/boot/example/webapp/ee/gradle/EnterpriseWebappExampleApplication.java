4edcc6c1ae20f7a2f104d90a2b9a5cde543f4272
Simon Zambrovski <simon@zambrovski.org>
2017-06-02 17:35:37 +0200

    Add new example for CE, fixed versions of artifacts (#256)

--- example-webapp-gradle/src/main/java/org/camunda/bpm/spring/boot/example/webapp/ee/gradle/EnterpriseWebappExampleApplication.java
+++ example-webapp-gradle/src/main/java/org/camunda/bpm/spring/boot/example/webapp/ee/gradle/EnterpriseWebappExampleApplication.java
@@ -1,13 +1,13 @@
 package org.camunda.bpm.spring.boot.example.webapp.ee.gradle;
 
+
 import org.camunda.bpm.application.ProcessApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@org.springframework.boot.autoconfigure.SpringBootApplication
 @ProcessApplication
 public class EnterpriseWebappExampleApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(EnterpriseWebappExampleApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseWebappExampleApplication.class, args);
+    }
 }
