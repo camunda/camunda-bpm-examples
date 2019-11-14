bd06e3f10f005c8496e24300dfa5d2870731f22e
Tobias Metzke <tobias.metzke@camunda.com>
2019-11-14 09:37:06 +0100

    chore(multi-tenancy): upgrade example to WildFly
    related to CAM-11016

--- multi-tenancy/schema-isolation/src/main/java/org/camunda/bpm/tutorial/multitenancy/TenantInterceptor.java
+++ multi-tenancy/schema-isolation/src/main/java/org/camunda/bpm/tutorial/multitenancy/TenantInterceptor.java
@@ -16,31 +16,26 @@
  */
 package org.camunda.bpm.tutorial.multitenancy;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
-
-import org.jboss.resteasy.annotations.interception.ServerInterceptor;
-import org.jboss.resteasy.core.ResourceMethod;
-import org.jboss.resteasy.core.ServerResponse;
-import org.jboss.resteasy.spi.Failure;
-import org.jboss.resteasy.spi.HttpRequest;
-import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 
 /**
  * @author Thorben Lindhauer
  *
  */
 @Provider
-@ServerInterceptor
-public class TenantInterceptor implements PreProcessInterceptor {
+public class TenantInterceptor implements ContainerRequestFilter {
 
-  protected static final Map<String, String> USER_TENANT_MAPPING = new HashMap<String, String>();
+  protected static final Map<String, String> USER_TENANT_MAPPING = new HashMap<>();
 
   static {
     USER_TENANT_MAPPING.put("kermit", "tenant1");
@@ -50,8 +45,9 @@ public class TenantInterceptor implements PreProcessInterceptor {
   @Inject
   protected Tenant tenant;
 
-  public ServerResponse preProcess(HttpRequest request, ResourceMethod method) throws Failure, WebApplicationException {
-    List<String> user = request.getUri().getQueryParameters().get("user");
+  @Override
+  public void filter(ContainerRequestContext requestContext) throws IOException {
+    List<String> user = requestContext.getUriInfo().getQueryParameters().get("user");
 
     if (user.size() != 1) {
       throw new WebApplicationException(Status.BAD_REQUEST);
@@ -59,7 +55,6 @@ public class TenantInterceptor implements PreProcessInterceptor {
 
     String tenantForUser = USER_TENANT_MAPPING.get(user.get(0));
     tenant.setId(tenantForUser);
-
-    return null;
   }
+
 }
