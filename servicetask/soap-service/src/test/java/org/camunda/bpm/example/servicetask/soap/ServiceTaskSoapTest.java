d251407a437082544843b97fc403603160fbd0f6
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-05-20 14:05:09 +0200

    fix(servicetask): mock backend of soap example
    related to CAM-6091

--- servicetask/soap-service/src/test/java/org/camunda/bpm/example/servicetask/soap/ServiceTaskSoapTest.java
+++ servicetask/soap-service/src/test/java/org/camunda/bpm/example/servicetask/soap/ServiceTaskSoapTest.java
@@ -16,48 +16,99 @@
  */
 package org.camunda.bpm.example.servicetask.soap;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-/**
- * @author Daniel Meyer
- *
- */
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.camunda.bpm.example.servicetask.soap.MockedSoapRule.MOCKED_BASE_URL;
+import static org.camunda.bpm.example.servicetask.soap.MockedSoapRule.MOCKED_HOST;
+import static org.camunda.bpm.example.servicetask.soap.MockedSoapRule.MOCKED_PORT;
+import static org.camunda.bpm.example.servicetask.soap.MockedSoapRule.MOCKED_SERVICE_PATH;
+import static org.camunda.bpm.example.servicetask.soap.MockedSoapRule.MOCKED_SOAP_ACTION;
+
 public class ServiceTaskSoapTest {
+
+  @ClassRule
+  public static MockedSoapRule mockedBackend = new MockedSoapRule(options()
+      .bindAddress(MOCKED_HOST)
+      .port(MOCKED_PORT));
 
   @Rule
   public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
+  protected RuntimeService runtimeService;
+  protected TaskService taskService;
+
+  @Before
+  public void assignServices() {
+    runtimeService = processEngineRule.getRuntimeService();
+    taskService = processEngineRule.getTaskService();
+  }
+
   @Test
   @Deployment(resources={"invokeSoapService.bpmn"})
-  public void shouldInvokeService() {
-    Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("city", "Berlin-Tempelhof");
+  public void shouldCallMockedServiceWithTemperature17() {
+    // given
+    String city = "Berlin-Tempelhof";
+    mockedBackend.response("response_temperature_17.xml", city);
+
+    Map<String, Object> variables = new HashMap<>();
+    variables.put("base_url", MOCKED_BASE_URL);
+    variables.put("service_path", MOCKED_SERVICE_PATH);
+    variables.put("soap_action", MOCKED_SOAP_ACTION);
+
+    variables.put("city", city);
     variables.put("country", "Germany");
 
-    RuntimeService runtimeService = processEngineRule.getRuntimeService();
-    TaskService taskService = processEngineRule.getTaskService();
-
+    // when
     runtimeService.startProcessInstanceByKey("weatherForecast", variables);
 
+    // then
     Task task = taskService.createTaskQuery().singleResult();
     Assert.assertNotNull(task);
 
-    int temperature = Integer.parseInt( taskService.getVariable(task.getId(), "temperature").toString() );
-    if(temperature >= 18) {
-      Assert.assertEquals("UserTask_1", task.getTaskDefinitionKey());
-    } else {
-      Assert.assertEquals("UserTask_2", task.getTaskDefinitionKey());
-    }
+    String temperature = (String) taskService.getVariable(task.getId(), "temperature");
+    Assert.assertEquals("17", temperature);
+    Assert.assertEquals("UserTask_2", task.getTaskDefinitionKey());
 
+  }
+
+  @Test
+  @Deployment(resources={"invokeSoapService.bpmn"})
+  public void shouldCallMockedServiceWithTemperature20() {
+    // given
+    String city = "Munich";
+    mockedBackend.response("response_temperature_20.xml", city);
+
+    Map<String, Object> variables = new HashMap<>();
+    variables.put("base_url", MOCKED_BASE_URL);
+    variables.put("service_path", MOCKED_SERVICE_PATH);
+    variables.put("soap_action", MOCKED_SOAP_ACTION);
+
+    variables.put("city", city);
+    variables.put("country", "Germany");
+
+    // when
+    runtimeService.startProcessInstanceByKey("weatherForecast", variables);
+
+    // then
+    Task task = taskService.createTaskQuery().singleResult();
+    Assert.assertNotNull(task);
+
+    String temperature = (String) taskService.getVariable(task.getId(), "temperature");
+    Assert.assertEquals("20", temperature);
+    Assert.assertEquals("UserTask_1", task.getTaskDefinitionKey());
   }
 
 }
