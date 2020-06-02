f63b10fa30f0c304da6cd82aaee26efef9951f31
Nikola Koevski <nikola.koevski@camunda.com>
2020-06-02 16:22:15 +0200

    chore(examples): add spring boot invoice example
    Related to CAM-11737
    Closes PR  (#41)

---
+++ spring-boot-starter/example-invoice/src/main/java/org/camunda/bpm/spring/boot/example/invoice/InvoiceApplication.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.spring.boot.example.webapp.ee;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
+import org.camunda.bpm.example.invoice.InvoiceProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class InvoiceApplication {
+
+  public static void main(String... args) {
+    // Avoid resetting URL stream handler factory
+    TomcatURLStreamHandlerFactory.disable();
+    SpringApplication.run(InvoiceApplication.class, args);
+  }
+
+  @Autowired
+  protected ProcessEngine processEngine;
+
+  protected InvoiceProcessApplication invoicePa = new InvoiceProcessApplication();
+
+  @PostConstruct
+  public void deployInvoice() {
+    ClassLoader classLoader = invoicePa.getClass().getClassLoader();
+
+    if(processEngine.getIdentityService().createUserQuery().list().isEmpty()) {
+      processEngine.getRepositoryService()
+          .createDeployment()
+          .addInputStream("invoice.v1.bpmn", classLoader.getResourceAsStream("invoice.v1.bpmn"))
+          .addInputStream("reviewInvoice.bpmn", classLoader.getResourceAsStream("reviewInvoice.bpmn"))
+          .deploy();
+    }
+  }
+
+  @EventListener
+  public void onPostDeploy(PostDeployEvent event) {
+    invoicePa.startFirstProcess(event.getProcessEngine());
+  }
+
+}
