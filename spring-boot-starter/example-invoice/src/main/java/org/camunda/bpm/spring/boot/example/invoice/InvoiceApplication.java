5dbb833a1849f42ecd8e56e454aebeebb1d94fd7
yanavasileva <yanavasileva@users.noreply.github.com>
2020-06-04 08:51:39 +0200

    fix(starter): fix java package

--- spring-boot-starter/example-invoice/src/main/java/org/camunda/bpm/spring/boot/example/invoice/InvoiceApplication.java
+++ spring-boot-starter/example-invoice/src/main/java/org/camunda/bpm/spring/boot/example/invoice/InvoiceApplication.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.spring.boot.example.webapp.ee;
+package org.camunda.bpm.spring.boot.example.invoice;
 
 import javax.annotation.PostConstruct;
 
