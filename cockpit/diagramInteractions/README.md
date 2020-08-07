5b8fc0419761743d1239bb5a4687e67a5581a275
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-08-07 13:30:12 +0200

    chore(cockpit): add cockpit plugins
    related to CAM-12255

---
+++ cockpit/diagramInteractions/README.md
@@ -0,0 +1,60 @@
+Diagram interactions with custom plugins
+========================================
+
+This example shows how you can achieve diagram interactions with custom plugins.
+Click on an activity in the BPMN diagram to apply a filter to the table.
+
+Built and tested against Camunda BPM version `7.14.0-alpha2`.
+
+![Screenshot](screenshot.png)
+
+
+Diagram Interaction
+-------------------
+
+To achieve diagram interaction, we will use 2 plugins scoped in one module.
+The first plugin registers at `cockpit.processDefinition.diagram.plugin` or another diagram plugin point and registers a listener on the viewer event bus. With the dispatched event we can call our own functions:
+```javascript
+render: viewer => {
+  viewer.get("eventBus").on("element.click", event => {
+    if (event.element.type.includes("Task")) {
+      cb(event.element);
+    }
+    // ...
+  });
+}
+```
+
+In the second plugin, we can create a callback function which the diagram plugin calls. In this example, we will apply a filter to a table depending on what task was clicked on.
+
+```javascript
+  render: (node, { processDefinitionId }) => {
+    // ...
+    cb = el => {
+      getUsertasks(el.id);
+    };
+  }
+```
+
+Integrate into Camunda Webapp
+-----------------------------
+
+Copy the `diagramInteractions.js` file into the `app/cockpit/scripts/` folder in your Camunda webapp distribution.
+For the Tomcat distribution, this would be `server/apache-tomcat-X.X.XX/webapps/camunda/app/cockpit/scripts/`.
+
+Add the following content to the `app/cockpit/scripts/config.js` file:
+
+```
+// …
+  customScripts: [
+    'scripts/diagramInteractions.js'
+  ]
+// …
+```
+
+After that start the server, login to Cockpit and navigate to the process definition view to check the result.
+
+License
+-------
+
+Use under terms of the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ cockpit/diagramInteractions/diagramInteractions.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const engineApi = document.querySelector("base").getAttribute("engine-api");
+const apiUrl = `${engineApi}/engine/default`;
+
+const renderTable = async (operationMap, node) => {
+  if (!Object.keys(operationMap).length) {
+    node.innerHTML = "No open Tasks";
+    return;
+  }
+
+  const table = document.createElement("table");
+  table.style = "table-layout: fixed; width: 100%;";
+  table.createTHead().innerHTML = `
+    <td>User</td>
+    <td>Activity ID</td>
+    <td>Open Task Count</td>`;
+
+  const body = table.createTBody();
+
+  for (const [user, tasks] of Object.entries(operationMap)) {
+    for (const [task, count] of Object.entries(tasks)) {
+      const row = document.createElement("tr");
+      const userCol = document.createElement("td");
+      const taskCol = document.createElement("td");
+      const countCol = document.createElement("td");
+
+      userCol.innerText = user;
+      taskCol.innerText = task;
+      countCol.innerText = count;
+
+      row.appendChild(userCol);
+      row.appendChild(taskCol);
+      row.appendChild(countCol);
+      body.appendChild(row);
+    }
+  }
+
+  node.innerHTML = "";
+  node.appendChild(table);
+};
+
+let cb = el => console.error("No callback defined: ", el);
+
+const diagramPlugin = {
+  id: "diagramPlugin",
+  pluginPoint: "cockpit.processDefinition.diagram.plugin",
+  priority: 5,
+  render: viewer => {
+    viewer.get("eventBus").on("element.click", event => {
+      if (event.element.type.includes("Task")) {
+        cb(event.element);
+      } else {
+        cb(false);
+      }
+    });
+  }
+};
+
+const tabPlugin = {
+  id: "tabPlugin",
+  pluginPoint: "cockpit.processDefinition.runtime.tab",
+  priority: 5,
+  label: "Open User Tasks",
+  render: (node, { processDefinitionId }) => {
+    async function getUsertasks(taskId) {
+      let result;
+      if (taskId) {
+        result = await fetch(
+          `${apiUrl}/task?processDefinitionId=${processDefinitionId}&taskDefinitionKey=${taskId}&maxResults=500`
+        );
+      } else {
+        result = await fetch(
+          `${apiUrl}/task?processDefinitionId=${processDefinitionId}&maxResults=500`
+        );
+      }
+
+      const json = await result.json();
+      const operationMap = {};
+      json.forEach(task => {
+        const assignee = task.assignee || "unassigned";
+        const operationPerUser = operationMap[assignee] || {};
+        operationPerUser[task.taskDefinitionKey] =
+          operationPerUser[task.taskDefinitionKey] || 0;
+        operationPerUser[task.taskDefinitionKey]++;
+        operationMap[assignee] = operationPerUser;
+      });
+
+      renderTable(operationMap, node);
+    }
+
+    getUsertasks();
+
+    cb = el => {
+      getUsertasks(el.id);
+    };
+  },
+  unmount: () => {
+    cb = () => {};
+  }
+};
+
+export default [tabPlugin, diagramPlugin];
