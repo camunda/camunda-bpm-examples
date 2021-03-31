689155400a35ffd7aad4a5d3f583b15565c5536f
Anton von Weltzien <anton.weltzien@camunda.com>
2021-03-31 16:59:25 +0200

    wip bump spring boot

--- deployment/spring-boot/src/test/java/org/camunda/bpm/example/CamundaSpringBootExampleApplicationTest.java
+++ deployment/spring-boot/src/test/java/org/camunda/bpm/example/CamundaSpringBootExampleApplicationTest.java
@@ -18,7 +18,7 @@ package org.camunda.bpm.example;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
