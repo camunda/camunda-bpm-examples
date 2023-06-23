0bd3d6691b1b316c556dd416fcc4910f5fcae8d9
Yana Vasileva <yana.vasileva@camunda.com>
2023-06-23 13:56:32 +0200

    web

--- spring-boot-starter/example-web/src/test/java/org/camunda/bpm/spring/boot/example/web/CamundaBpmRestTest.java
+++ spring-boot-starter/example-web/src/test/java/org/camunda/bpm/spring/boot/example/web/CamundaBpmRestTest.java
@@ -16,23 +16,20 @@
  */
 package org.camunda.bpm.spring.boot.example.web;
 
+import static org.junit.Assert.assertEquals;
+
 import org.camunda.bpm.engine.rest.dto.repository.ProcessDefinitionDto;
 import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { RestApplication.class, RestConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
